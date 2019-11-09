f52c40281a83aa5509a6a97088cf9b5d714e56fa
Cheh91 <4eh91@ukr.net>
2019-11-09 20:42:45 +0200

    added version one

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,27 @@
 $(function(){
 
-    $('.slider__wrapper').slick({
+    $('.header__box-slider').slick({
         dots: true,
-        fade: true,
+        // fade: true,
         autoplay: true,
+        speed: 2000,
+        infinite: true,
+        
+        
+    });
+
+    $('.slider__photo').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+    $('.news__slider').slick({
+        dots: true,
+        // fade: true,
+        // autoplay: true,
         speed: 3000,
-        
-        
     });
 
 });
